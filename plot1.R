26b88a31d3fe2c65e67875c36ccf15f1a521cd24
karenjyang <karenjyang.1@gmail.com>
2014-05-10 19:04:20 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -21,4 +21,5 @@ png("plot1.png", width = 480, height = 480)
 str(data1$Global_active_power)
 hist(data1$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
+
 sqldf() # close connection
